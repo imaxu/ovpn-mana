3995daa7e03926065a0866f24b587f1423d03500
程序菌 <imaxu@sina.com>
2025-04-24 16:57:37 +0800

    update

--- build/CMakeFiles/openvpnmgr.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for openvpnmgr.
-# This may be replaced when dependencies are built.
--- include/config.hpp
+++ include/config.hpp
@@ -2,7 +2,7 @@
 
 #include <string>
 
-const std::string EASY_RSA_DIR = "/home/xuwh/easy-rsa";
+const std::string EASY_RSA_DIR = "/root/easy-rsa";
 const std::string OVPN_DIR = "/etc/openvpn";
 const std::string OPENVPN_BIN = "/usr/sbin/openvpn";
 const std::string SYSTEMCTL_BIN = "/bin/systemctl";
